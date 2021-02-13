fd027b2c7dcd10cd5a69283b331b4f1cb9f56d47
Mario Rubio <mario@mrrb.eu>
2021-02-13 17:27:57 +0100

    Working ZMOD and SCD30 test

--- src/main.c
+++ src/main.c
@@ -4,84 +4,146 @@
  * \brief 
  * \version 0.1
  * \date 2020-12-13
- * 
- * @copyright Copyright (c) 2020
- * 
  */
 
+#include <osapi.h>
 #include <c_types.h>
+#include <inttypes.h>
+
+#include "user_config.h"
+#include "fast_gpio.h"
 #include "uc_init.h"
+#include "sensors.h"
+
 #include "f2c/f2c.h"
-#include "scd30/scd30.h"
+
+#include "zmod4xxx/iaq_2nd_gen.h"
+#include "zmod4xxx/zmod4xxx_types.h"
 
 
-static volatile os_timer_t timer1;
-static volatile os_timer_t timer2;
+static zmod4xxx_dev_t zmod_dev;
+static iaq_2nd_gen_handle_t iaq_handle;
+static iaq_2nd_gen_results_t iaq_results;
+
+static scd30_result_t scd30_result;
+
+static uint8_t zmod4410_data_valid;
+static uint8_t scd30_data_valid;
+
+static volatile os_timer_t timer_blink;
+static volatile os_timer_t timer_scd30;
+static volatile os_timer_t timer_zmod;
+
 
 void ICACHE_FLASH_ATTR
-blink(void *arg) {
-    GPIO2_IN ? GPIO2_L : GPIO2_H;
-    // os_printf("Now I'm %d\r\n", GPIO2_IN);
+print_scd30_results() {
+    char* txt_buff = os_zalloc(sizeof(char) * F2C_CHAR_BUFF_SIZE);
+    char* txt_f2c;
+
+    txt_f2c = f2c(*((real32_t*) &(scd30_result.co2)), txt_buff);
+    os_printf("CO2: %s ppm\n", txt_f2c);
+
+    txt_f2c = f2c(*(real32_t*) &scd30_result.temp, txt_buff);
+    os_printf("T:   %s C\n", txt_f2c);
+
+    txt_f2c = f2c(*(real32_t*) &scd30_result.rh, txt_buff);
+    os_printf("RH:  %s %%\n\n", txt_f2c);
+
+    os_free(txt_buff);
 }
 
 void ICACHE_FLASH_ATTR
-test_read(void *arg) {
-    uint8_t status = 0;
-    uint8_t rdy = 0;
-    uint8_t fw_major, fw_minor;
-    uint32_t co2, t, rh;
-
-    char* txt_buff = os_malloc(sizeof(char)*F2C_CHAR_BUFF_SIZE);
+print_zmod_results() {
+    char* txt_buff = os_zalloc(sizeof(char) * F2C_CHAR_BUFF_SIZE);
     char* txt_f2c;
 
-    for (size_t i = 0; i < F2C_CHAR_BUFF_SIZE; ++i) {
-        txt_buff[i] = 0;
-    }
+    txt_f2c = f2c((real32_t) iaq_results.eco2, txt_buff);
+    os_printf("eCO2: %s ppm\n", txt_f2c);
 
-    status = scd30_get_firmware_version(&fw_major, &fw_minor);
-    os_printf("FW:  %d.%d (%d)\n", fw_major, fw_minor, status);
+    txt_f2c = f2c((real32_t) iaq_results.etoh, txt_buff);
+    os_printf("eTOH: %s ppm\n", txt_f2c);
 
-    status = scd30_check_data_ready(&rdy);
-    if (rdy) {
-        os_printf("Measure ready.\n", rdy, status);
-        status = scd30_read_measurement(&co2, &t, &rh);
+    txt_f2c = f2c((real32_t) iaq_results.iaq, txt_buff);
+    os_printf("IAQ: %s\n", txt_f2c);
 
-        txt_f2c = f2c(*(real32_t*) &co2, txt_buff);
-        os_printf("CO2: %s (%d)\n", txt_f2c, status);
+    txt_f2c = f2c((real32_t) iaq_results.tvoc, txt_buff);
+    os_printf("TVOC: %s mg/m^3\n", txt_f2c);
 
-        txt_f2c = f2c(*(real32_t*) &t, txt_buff);
-        os_printf("T:   %s (%d)\n", txt_f2c, status);
-
-        txt_f2c = f2c(*(real32_t*) &rh, txt_buff);
-        os_printf("RH:  %s (%d)\n\n", txt_f2c, status);
-    } else {
-        os_printf("Measure NOT ready.\n\n", rdy, status);
-    }
-
+    txt_f2c = f2c((real32_t) iaq_results.tvoc, txt_buff);
+    os_printf("log_Rcda: %s logOhm\n\n", txt_f2c);
 
     os_free(txt_buff);
+}
+
+void ICACHE_FLASH_ATTR
+timer_func_blink(void* args) {
+    GPIO2_IN ? GPIO2_L : GPIO2_H;
+}
+
+void ICACHE_FLASH_ATTR
+timer_func_scd30(void* args) {
+    sensor_status_t result;
+    result = read_scd30(&scd30_result);
+
+    scd30_data_valid = 0;
+    if (result == SENSOR_READ_VALID) {
+        print_scd30_results();
+        scd30_data_valid = 1;
+    }
+}
+
+void ICACHE_FLASH_ATTR
+timer_func_zmod(void* args) {
+    sensor_status_t result;
+
+    os_timer_disarm((os_timer_t*)&timer_zmod);
+    result = read_zmod(&zmod_dev, &iaq_handle, &iaq_results);
+
+    zmod4410_data_valid = 0;
+    if (result == SENSOR_READ_VALID) {
+        print_zmod_results();
+        zmod4410_data_valid = 1;
+    } else if (result == SENSOR_ZMOD_STABILIZATION) {
+        os_printf("ZMOD stabilization process!\n");
+    } else {
+        os_printf("ZMOD critical error %d\n", result);
+    }
+
+    os_timer_arm((os_timer_t*)&timer_zmod, ZMOD_READ_INTERVAL, 1);
 }
 
 void ICACHE_FLASH_ATTR
 user_pre_init() {}
 
 void ICACHE_FLASH_ATTR
-user_init()
-{
-    uc_init_uart();
-    uc_init_wifi();
-    uc_init_i2c();
+user_init() {
+    status_t status;
 
-    // espconn_gethostbyname();
-    // ICACHE_RODATA_ATTR
+    status = uc_init_uart();
+    status = uc_init_wifi();
+    status = uc_init_gpio();
+    status = uc_init_i2c();
+    status = uc_init_sensors(&zmod_dev, &iaq_handle);
+    read_zmod(&zmod_dev, &iaq_handle, &iaq_results);
 
-    gpio_init();
-    GPIO2_OUTPUT_SET;
+    if (status != STA_OK) {
+#ifdef DEBUG_MODE
+        os_printf("uc init failed, please restart!\n");
+#endif
+        for (;;) {
+            os_delay_us(1000);
+        }
+    }
 
-    // User tasks? system_os_task(...)
-    // os_timer_setfn((os_timer_t*)&timer1, (os_timer_func_t *)blink, NULL);
-    // os_timer_arm((os_timer_t*)&timer1, 500, 1);
+    // Timers
+    os_timer_setfn((os_timer_t*)&timer_blink, (os_timer_func_t *)timer_func_blink, NULL);
+#ifdef STATUS_LED_ENABLE
+    os_timer_arm((os_timer_t*)&timer_blink, STATUS_LED_TIME, 1);
+#endif
 
-    os_timer_setfn((os_timer_t*)&timer2, (os_timer_func_t *)test_read, NULL);
-    os_timer_arm((os_timer_t*)&timer2, 3000, 1);
+    os_timer_setfn((os_timer_t*)&timer_scd30, (os_timer_func_t *)timer_func_scd30, NULL);
+    os_timer_arm((os_timer_t*)&timer_scd30, SCD30_READ_INTERVAL, 1);
+
+    os_timer_setfn((os_timer_t*)&timer_zmod, (os_timer_func_t *)timer_func_zmod, NULL);
+    os_timer_arm((os_timer_t*)&timer_zmod, ZMOD_READ_INTERVAL, 1);
 }
