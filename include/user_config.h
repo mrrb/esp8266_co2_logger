c9635c342ec770d01c0956ed270446d69e9e1a6c
Mario Rubio <mario@mrrb.eu>
2021-02-13 17:07:33 +0100

    Add new config entries

--- include/user_config.h
+++ include/user_config.h
@@ -1,9 +1,14 @@
+// Debug
+#ifndef DEBUG_MODE
+#define DEBUG_MODE
+#endif
+
 // UART
 #define	BITRATE 115200
 
 // Network
-#define SSID    "mrrb_dev"
-#define SSID_PW "WhatTimeIsIt?AdventureTime!-_-"
+#define SSID     "mrrb_dev"
+#define SSID_PW  "WhatTimeIsIt?AdventureTime!-_-"
 #define HOSTNAME "ESP-CO2-logger"
 
 #define SSID_LENGTH     16
@@ -12,6 +17,15 @@
 
 // I2C
 // View header $(PROJECT_ROOT)/driver/driver/i2c_master.h
+
+// Status LED
+#define STATUS_LED_ENABLE
+#define STATUS_LED_TIME   2000
+
+// Sensors
+#define SCD30_READ_INTERVAL   3000
+#define ZMOD_READ_INTERVAL    1990
+#define SERVER_WRITE_INTERVAL 5000
 
 // Other
 #define USE_OPTIMIZE_PRINTF
